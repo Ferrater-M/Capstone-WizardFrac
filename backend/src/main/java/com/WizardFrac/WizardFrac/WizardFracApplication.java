3603f9463d9b22fccab6564b3bfb5d7c5ed932a2
Elaiza Jane Moreno <Elaizajanemoreno3@gmail.com>
2026-05-21 23:38:14 +0800

    add backend and read.md

---
+++ backend/src/main/java/com/WizardFrac/WizardFrac/WizardFracApplication.java
@@ -0,0 +1,13 @@
+package com.WizardFrac.WizardFrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardFracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizardFracApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/WizardFrac/WizardFrac/WizardFracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WizardFrac.WizardFrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardFracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
